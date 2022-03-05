4b9b3f50fc168080efdd4a8af4d92960b45559c9
Traversie Thomas <thomas.traversie@student-cs.fr>
2022-03-05 14:56:40 +0100

    Part 1

--- src/main/scala/shared/predictions.scala
+++ src/main/scala/shared/predictions.scala
@@ -45,49 +45,33 @@ package object predictions
   }
 
   def meanRatingUser(user : Int, ratings : Seq[Rating]) : Double = {
-    var ratings_u = Seq.empty[Double]
-    for (r_u_i <- ratings) {
-      if (r_u_i.user == user) ratings_u :+ r_u_i.rating
+        mean(ratings.filter(_.user == user).map{case Rating(u,i,r) => r})
     }
-    mean(ratings_u)
-  }
 
-  def meanRatingItem(item : Int, ratings : Seq[Rating]) : Double = {
-    var ratings_i = Seq.empty[Double]
-    for (r_u_i <- ratings) {
-      if (r_u_i.item == item) ratings_i :+ r_u_i.rating
-    }
-    mean(ratings_i)
+    def meanRatingItem(item : Int, ratings : Seq[Rating]) : Double = {
+        mean(ratings.filter(_.item == item).map{case Rating(u,i,r) => r})
     }
 
     def meanRatings(ratings : Seq[Rating]) : Double = {
-      var ratings_val = Seq.empty[Double]
-      for (r <- ratings) {
-        if (r.rating != -1) ratings_val :+ r.rating
-      }
-      mean(ratings_val)
+       mean(ratings.filter(_.rating != -1).map{case Rating(u,i,r) => r}) 
     }
 
     def scale(x : Double, y : Double) : Double = 
-      if (x > y) 5-y else if (x < y) y-1 else 1
+        if (x > y) 5-y else if (x < y) y-1 else 1
 
     def normalize(r_u_i : Double, user : Int, item : Int, ratings : Seq[Rating]) : Double = {
-      val r_u = meanRatingUser(user, ratings)
-      (r_u_i - r_u)/scale(r_u_i, r_u)
+        val r_u = meanRatingUser(user, ratings)
+        (r_u_i - r_u)/scale(r_u_i, r_u)
     }
 
     def meanNormalizedItem(item : Int, ratings : Seq[Rating]) : Double = {
-      var ratings_i = Seq.empty[Double]
-      for (r <- ratings) {
-        if (r.item == item) ratings_i :+ normalize(r.rating, r.user, item, ratings)
-      }
-      mean(ratings_i)
+        mean(ratings.filter(_.item == item).map{case Rating(u,i,r) => normalize(r, u, item, ratings)})
     }
 
     def prediction(item : Int, user : Int, ratings : Seq[Rating]) : Double = {
-      if (meanRatingUser(user, ratings) == 0.0) meanRatings(ratings)
-      val r_u = meanRatingUser(user, ratings)
-      val r_i = meanNormalizedItem(item, ratings)
-      r_u + r_i * scale(r_u + r_i, r_u)
+        if (meanRatingUser(user, ratings) == 0.0) meanRatings(ratings)
+        val r_u = meanRatingUser(user, ratings)
+        val r_i = meanNormalizedItem(item, ratings)
+        r_u + r_i * scale(r_u + r_i, r_u)
     }
 }
